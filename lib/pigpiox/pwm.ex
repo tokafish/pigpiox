8d361f48a34318a4791ce9effdc39637e1897f13
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-11-22 12:26:01 -0500

    Specify supported level ranges for PWM

--- lib/pigpiox/pwm.ex
+++ lib/pigpiox/pwm.ex
@@ -6,12 +6,12 @@ defmodule Pigpiox.Pwm do
   @moduledoc """
   Build and send waveforms with pigpiod.
   """
-  
+
   @doc """
-  Sets the current dutycycle and fequency for the hardware PWM on a specific GPIO `pin`
+  Sets the current dutycycle and fequency for the hardware PWM on a specific GPIO `pin`. Max value for `level` is `1_000_000`.
   """
   @spec hardware_pwm(pin :: integer, freq :: integer, level :: integer) :: :ok | {:error, atom}
-  def hardware_pwm(pin, freq, level) do
+  def hardware_pwm(pin, freq, level) when level in 0..1_000_000 do
     case Pigpiox.Socket.command(:hardware_pwm, pin, freq, [level]) do
       {:ok, _} -> :ok
       error -> error
@@ -19,10 +19,10 @@ defmodule Pigpiox.Pwm do
   end
 
   @doc """
-  Sets up a specific pin as a software PWM and sets the current dutycycle
+  Sets up a specific pin as a software PWM and sets the current dutycycle. Max value for `level` is `255`.
   """
   @spec gpio_pwm(pin :: integer, level :: integer) :: :ok | {:error, atom}
-  def gpio_pwm(pin, level) do
+  def gpio_pwm(pin, level) when level in 0..255 do
     case Pigpiox.Socket.command(:gpio_PWM, pin, level) do
       {:ok, _} -> :ok
       error -> error
@@ -62,4 +62,3 @@ defmodule Pigpiox.Pwm do
     end
   end
 end
-
